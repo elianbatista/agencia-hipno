dc4cae1bbdc8f5a4228ef56a9fdb9829c87e9304
Elian Batista <elianbatista2000@gmail.com>
2021-02-07 21:25:35 -0300

    alterando a index

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 import LateralMenu from '../../components/LateralMenu'
 import Topo from '../../components/Topo'
@@ -13,16 +13,79 @@ import Footer from '../../components/Footer'
 
 const Index: React.FC = () => {
   const windowSize = window.innerHeight
+  const wrapper: any = useRef(null)
 
   const [menu, setMenu] = useState(false)
   const [menuBlack, setMenuBlack] = useState(false)
-  const [scroll, setScroll] = useState(0)
+  const [scroll, setScroll] = useState(0) // Valor do Scroll no Wrapper
+  const [scrollTo, setScrollTo] = useState('') // Nome da section para scrollar
   const [contato, setContato] = useState(false)
+  const [positionSobreSection, setPositionSobreSection]: any = useState(null)
+  const [positionServicosSection, setPositionServicosSection]: any = useState(null)
+
+  const [itensMenu, setItensMenu] = useState([
+    {
+      id: 1,
+      titulo: 'Home',
+      class: 'home',
+      active: true
+    },
+    {
+      id: 2,
+      titulo: 'Sobre',
+      class: 'sobre',
+      active: false
+    },
+    {
+      id: 3,
+      titulo: 'Serviços',
+      class: 'servicos',
+      active: false
+    },
+    {
+      id: 5,
+      titulo: 'Portfolio',
+      class: 'portfolio',
+      active: false
+    }
+  ])
+
+  useEffect(() => {
+    console.log(wrapper.current?.offsetHeight)
+    if (wrapper != null) {
+      setPositionServicosSection(wrapper.current?.querySelector('section.servicos').getBoundingClientRect())
+      setPositionSobreSection(wrapper.current?.querySelector('section.sobre').getBoundingClientRect())
+    }
+  }, [wrapper])
+
+  useEffect(() => {
+    console.log(positionSobreSection)
+  }, [positionSobreSection])
 
   function handleScrollWrapper(event: any) {
     setScroll(event.target.scrollTop)
-    
+    console.log(scroll)
     scrollSetPage(scroll)
+  }
+
+  function handleMenu(id: number) {
+    const newMenu = itensMenu.map(item => {
+      if (item.id === id) {
+        if (item.class === 'home') {
+          handleScrollTo(0)
+        } else if (item.class === 'servicos') {
+          handleScrollTo(wrapper.current?.offsetHeight + positionServicosSection?.y - positionServicosSection?.height)
+        } else if (item.class === 'sobre') {
+          handleScrollTo(wrapper.current?.offsetHeight + positionSobreSection?.y - positionSobreSection?.height)
+        }
+      }
+      return item.id === id ? { ...item, active: true } : { ...item, active: false }
+    })
+    setItensMenu(newMenu)
+  }
+
+  function handleScrollTo(position: number) {
+    wrapper.current?.scrollTo(0, position)
   }
 
   function scrollSetPage(scrollValue: number) {
@@ -43,11 +106,11 @@ const Index: React.FC = () => {
     <div className="container">
       <Contato active={contato} />
       <div className={contato ? 'background-gray active' : 'background-gray'} onClick={() => setContato(false)}></div>
-      <LateralMenu active={menu} black={menuBlack} />
-      <div  className={contato ? 'wrapper no-scroll' : 'wrapper'} onScroll={handleScrollWrapper}>
-        <Topo setContato={setContato} />
+      <LateralMenu active={menu} black={menuBlack} menu={itensMenu} handleMenu={handleMenu} />
+      <div  className={contato ? 'wrapper no-scroll' : 'wrapper'} onScroll={handleScrollWrapper} ref={wrapper}>
+        <Topo menu={itensMenu} handleMenu={handleMenu} setContato={setContato} />
         <Banner />
-        <Container scroll={scroll} />
+        <Container scroll={scroll} scrollTo={scrollTo} />
         <Footer />
       </div>
     </div>
