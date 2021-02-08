2a5d75dbfbb5c6b3a40f99af34694c4b3e82d50f
Elian Batista <elianbatista2000@gmail.com>
2021-02-08 20:54:47 -0300

    alteracao na config de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Index from './pages/Index';
+import Servicos from './pages/Servicos';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
-        <Route path ='/' component={Index} />
+        <Route exact path ='/' component={Index} />
+        <Route path ='/servicos' component={Servicos} />
       </Switch>
     </Router>
   )
